e880d667026b4e05f410f842031d04d96fd488e1
gerasantillan17 <gerapumas122@gmail.com>
2024-11-07 00:09:36 -0600

    Add files via upload

---
+++ EVA2_16_ARREGLOS_DIASII.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_16_arreglos_diasii;
+
+/**
+ *
+ * @author gerar
+ */
+public class EVA2_16_ARREGLOS_DIASII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[]diasSemana = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+        for (int i=0; i < diasSemana.length; i++){
+        System.out.println(diasSemana[i]);
+    }
+    
+}
+}
